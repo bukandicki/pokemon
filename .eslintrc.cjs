456c75efb33f751098befd9d344963e6b4fffa83
Dicki <bukandicki@gmail.com>
2023-05-03 23:14:20 +0700

    update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,22 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  }
-}
+  },
+  overrides: [
+    {
+      files: ['./src/views/**/*.vue', './src/components/**/*.vue'],
+      rules: {
+        'vue/multi-word-component-names': 'off'
+      }
+    }
+  ]
+};
