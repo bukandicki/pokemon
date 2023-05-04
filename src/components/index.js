0ff7949d2f623eb6e84e8437c1a4d182f76f1457
Dicki <bukandicki@gmail.com>
2023-05-04 22:55:11 +0700

    add reusable Like component

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,13 @@
 import GridList from './GridList/GridList.component.vue';
 import Label from './Label/Label.component.vue';
+import Like from './Like/Like.component.vue';
 import Navbar from './Navbar/Navbar.component.vue';
 import PokemonCard from './PokemonCard/PokemonCard.component.vue';
 
 export const installGlobalComponents = (app) => {
   app.component('jublia-grid', GridList);
   app.component('jublia-label', Label);
+  app.component('jublia-like', Like);
   app.component('jublia-nav', Navbar);
   app.component('jublia-card', PokemonCard);
 };
